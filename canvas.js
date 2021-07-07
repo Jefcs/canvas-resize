b04ff92d70efeb93a019ec91a1fbc80071b76091
Jefcs <jeffersonc.silva@hotmail.com>
2021-07-07 15:01:11 -0300

    Better color and risezing screen features added

--- canvas.js
+++ canvas.js
@@ -1,9 +1,9 @@
 var canvas = document.querySelector('canvas');
 
+var c = canvas.getContext('2d');
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
-var c = canvas.getContext('2d');
 
 var mouse = {
     x: undefined,
@@ -11,13 +11,19 @@ var mouse = {
 };
 
 var maxRadius = 40;
-var minRadius = 2;
 
-var colorArray = ['blue', 'red', 'black', 'orange', 'yellow'];
+var colorArray = ['#0339A6', '#04C4D9', '#F2B705', 'black', '#F2380F'];
 
 window.addEventListener('mousemove', function (event) {
     mouse.x = event.x;
     mouse.y = event.y;
+});
+
+window.addEventListener('resize', function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    init();
 });
 
 function Circle(x, y, dx, dy, radius) {
@@ -26,6 +32,7 @@ function Circle(x, y, dx, dy, radius) {
     this.dx = dx;
     this.dy = dy;
     this.radius = radius;
+    this.minRadius = radius;
     this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
 
     this.draw = function () {
@@ -57,7 +64,7 @@ function Circle(x, y, dx, dy, radius) {
             if (this.radius < maxRadius) {
                 this.radius += 1;
             }
-        } else if (this.radius > minRadius) {
+        } else if (this.radius > this.minRadius) {
             this.radius -= 1;
         }
 
@@ -67,13 +74,16 @@ function Circle(x, y, dx, dy, radius) {
 
 var circleArray = [];
 
-for (var i = 0; i < 600; i++) {
-    var x = Math.random() * (innerWidth - radius * 2) + radius;
-    var y = Math.random() * (innerHeight - radius * 2) + radius;
-    var dx = (Math.random() - 0.5) * 4;
-    var dy = (Math.random() - 0.5) * 4;
-    var radius = 30;
-    circleArray.push(new Circle(x, y, dx, dy, radius));
+function init() {
+    circleArray = [];
+    for (var i = 0; i < 800; i++) {
+        var radius = Math.random() * 3 + 1;
+        var x = Math.random() * (innerWidth - radius * 2) + radius;
+        var y = Math.random() * (innerHeight - radius * 2) + radius;
+        var dx = Math.random() - 0.5;
+        var dy = Math.random() - 0.5;
+        circleArray.push(new Circle(x, y, dx, dy, radius));
+    }
 }
 
 function animate() {
@@ -86,3 +96,4 @@ function animate() {
 }
 
 animate();
+init();
